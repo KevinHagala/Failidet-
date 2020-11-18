23aa134d36265d1d4b49aea0ff3e05df7944b74d
Opilane TTHK <opilane@tthk.ee>
2020-11-18 15:13:55 +0200

    Create web Page

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\sample";
+            DeleteAllfiles();
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootPath);
+            foreach(DirectoryInfo dir in rootDirectory.GetDirectories())
+            {
+                DeleteAllfolders(dir.FullName, true);
+            }
+            
+        }
+
+            // a function to delete all files
+
+            public static void DeleteAllfiles()
+            {
+                string rootPath = @"C:\Users\opilane\sample";
+                DirectoryInfo directory = new DirectoryInfo(rootPath);
+                
+                foreach(FileInfo file in directory.GetFiles())
+                {
+                    file.Delete();
+                }
+            }
+        //a function to delete all folders
+
+        public static void DeleteAllfolders(string DirectoryName, bool ifExists)
+        {
+            if (Directory.Exists(DirectoryName))
+            {
+                Directory.Delete(DirectoryName, true);
+            }else if (ifExists)
+            {
+                throw new SystemException("No such directory to delete");
+            }
+
+        }
+    }
+}
+
---
+++ createDirectory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace createDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void CreateFolderOne()
+        {
+            string rootPath = @"C:\Users\opilane\sample";
+            string folderName = "playerOne;";
+            string fullFolderPath = Path.Combine(rootPath, folderName);
+            Console.WriteLine(fullFolderPath);
+            if (Directory.Exists(fullFolderPath)) // check if folder exists
+            {
+                Console.WriteLine("The folder already exists");
+            }else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+        public static void CreateFolderTwo()
+        {
+
+        }
+    }
+}
